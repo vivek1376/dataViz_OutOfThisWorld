f7156357b58fad3a7db5d7cea38b0295662ac142
Vivek Mehra <vivek.1376@gmail.com>
2023-02-19 02:09:29 -0500

    create bar chart for num planets vs. num stars

--- js/barChart.js
+++ js/barChart.js
@@ -1,18 +1,22 @@
 class barChart {
 
-    constructor(_config) {
+    constructor(_config, _data) {
         this.config = {
             parentElement: _config.parentElement,
-            containerWidth: _config.containerWidth || 500,
-            containerHeight: _config.containerHeight || 140,
+            containerWidth: _config.containerWidth || 600,
+            containerHeight: _config.containerHeight || 400,
             margin: { top: 10, bottom: 30, right: 10, left: 30 }
         }
 
         // Call a class function
+
+        this.data = _data;
         this.initVis();
     }
 
     initVis() {
+
+        let vis = this; 
 
         // Width and height as the inner dimensions of the chart area- as before
         vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
@@ -24,9 +28,48 @@ class barChart {
             .attr('width', vis.config.containerWidth)
             .attr('height', vis.config.containerHeight)
 
+        vis.chart = vis.svg.append('g')
+            .attr('transform', `translate(${vis.config.margin.left}, 
+                ${vis.config.margin.top})`);
+
+        // Initialize linear and ordinal scales (input domain and output 
+        // range)
+        const xScale = d3.scaleBand()
+            .domain(Object.keys(vis.data))
+            .range([0, vis.width])
+            .paddingInner(0.1);
+
+        // console.log("xscale test");
+        // console.log(xScale("2"));
+        // console.log(xScale("4"));
+
+        const yScale = d3.scaleLinear()
+        .domain([0, d3.max(Object.values(vis.data))])
+        .range([0, vis.height]);
 
 
-        updateVis(); //leave this empty for now...
+        console.log(vis.data);
+        const arrData = Object.entries(vis.data);
+
+        console.log(arrData);
+
+        vis.chart.selectAll('rect')
+        .data(arrData)
+        .enter()
+        .append('rect')
+        .attr('fill', '#a6324c')
+            .attr('width', xScale.bandwidth())
+            // .attr('width', d => xScale(parseInt(d[0])))
+            // .attr('height', function(d) {
+            //     console.log("inside attr");
+            //     console.log(d);
+            // })
+            .attr('height', d => yScale(d[1]))
+            .attr('x', d => xScale(d[0]))
+            .attr('y', d => (vis.height - yScale(d[1])));
+
+        
+        // updateVis(); //leave this empty for now...
     }
 
 
--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("hello");
     // d3.select("body").append("p").text("Hello World!");
 
+    var numPlanetsVsStars = {};
 
     d3.csv('data/exoplanets-1.edited.style.csv')
         .then(data => {
@@ -15,21 +16,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 console.log(typeof(val) + `  ${key} : ` + val);
             }
 
-            var numPlanetsVsStars = {};
-            
+            // console.log("numPlanet v stars");
+            // console.log(numPlanetsVsStars);
+
             for (const planetData of data) {
                 const numStars = planetData['sy_snum'];
-                console.log(`numStarts: ${numStars}` + " " + typeof(numStars));
+                // console.log(`numStarts: ${numStars}` + " " + typeof(numStars));
 
                 if (!numPlanetsVsStars.hasOwnProperty(numStars)) {
-                    console.log("not found");
                     numPlanetsVsStars[numStars] = 0;
                 }
 
                 numPlanetsVsStars[numStars]++;
             }
 
-            console.log(numPlanetsVsStars);
+            // console.log("numPlanet v stars");
+            // console.log(numPlanetsVsStars);
+
+            // Create an instance (for example in main.js)
+            let planetChart = new barChart ({
+                'parentElement': '#vizcontainer',
+                'containerHeight': 400,
+                'containerWidth': 600
+            }, numPlanetsVsStars);
+
         })
         .catch(error => {
             console.error('Error loading the data: ' + error);
@@ -57,4 +67,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // create 'p' elements after body
     // let p2 = d3.select('body').data(iceCreamFlavors).enter()
     //     .append('p');
+
+
+
+
+
+
+
+
+
+
 });
